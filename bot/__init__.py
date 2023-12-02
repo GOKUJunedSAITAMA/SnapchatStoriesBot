7e2c67f70e0245d42c736f04577d8a24eda1aea2
GOKUJunedSAITAMA <151656379+GOKUJunedSAITAMA@users.noreply.github.com>
2023-12-02 17:30:57 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -12,7 +12,7 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 class ENV_VARS(object):
     API_ID = int(os.environ.get("API_ID", "27385225"))
     API_HASH = os.environ.get("API_HASH", "841ac6a2c18172854b93bcfdadfdebb5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5820508322:AAGqmvdR5xg044Ua_op83LDBTMy48ZiaHG8")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6320915288:AAHaLrQ08NeQj3pbD6K9kskIjs8oUJ5_1_A")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "JunedMarkINFINITY_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
